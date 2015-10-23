58a6d90d1628ee2f458128a558627e47b2e3b088
Md. Shohanur Rahaman <shohan4556@gmail.com>
2015-10-23 10:13:15 +0600

    add Score

---
+++ script/ui/Score.js
@@ -0,0 +1,25 @@
+var Score = function(game){
+    
+};
+
+
+    Score.prototype = {
+      
+        create: function(){
+                
+        },
+        
+        createScore: function(){
+            this.scoreFont = "50px Arial";
+            this.scoreLabel = this.game.add.text(game.world.centerX);
+            
+        },
+        
+        update: function(){
+            
+        },
+        
+        render: function(){
+            
+        }
+    };
