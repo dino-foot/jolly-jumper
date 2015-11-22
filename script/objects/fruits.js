bf880a1982683ba93034b0d01a8de44ff7cd3382
Md. Shohanur Rahaman <shohan4556@gmail.com>
2015-11-22 20:17:42 +0600

    life sprite fixed

--- script/objects/fruits.js
+++ script/objects/fruits.js
@@ -1,6 +1,6 @@
 var fruits = function(game){
     this.fruitYMin = 99999;
-    this.coconutLoopTime = 10000;
+    this.coconutLoopTime = 5000;
 };
 
 /*
@@ -18,6 +18,8 @@ var fruits = function(game){
              this.coconutGroup.callAll('anchor.setTo','anchor',0.5,0.5);
              this.coconutGroup.callAll('body.gravity.set','body.gravity',0,400);
              this.coconutGroup.callAll('body.bounce.set','body.bounce',0.8);
+             this.coconutGroup.setAll('body.checkCollision.left',false); 
+             this.coconutGroup.setAll('body.checkCollision.right',false); 
             
             var fruitsArray = new Array('fruit0','fruit1','fruit2','fruit3','fruit4');
             this.fruitsGroup = game.add.physicsGroup(Phaser.Physics.ARCADE);
@@ -46,16 +48,18 @@ var fruits = function(game){
             Phaser.ArrayUtils.shuffle(this.fruitsGroup);
             this.fruitsGroup.updateZ();
             
-            game.time.events.loop(this.coconutLoopTime,this.handleCoconut,this)
+            game.time.events.loop(this.coconutLoopTime,this.handleCoconut,this);
         },
         
         handleCoconut: function(){
+            for(var i=0;i<2;i++){
             var coco = this.coconutGroup.getFirstDead();
             if(coco){ 
-                var x = game.rnd.integerInRange(10,310);
+                var x = game.rnd.integerInRange(60,300);
                 var y = this.y-200;
-                coco.reset(x,y);
+                coco.reset(x,y+100);
                 return;
+            }
             }
         },
     
