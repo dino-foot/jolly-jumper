6b0f89019074fae2b003270fc38aeedf1ac64a75
Md. Shohanur Rahaman <shohan4556@gmail.com>
2015-11-25 10:49:36 +0600

    level Rank

--- script/objects/fruits.js
+++ script/objects/fruits.js
@@ -9,6 +9,14 @@ var fruits = function(game){
     this.cocoLoopTime = 15000; // 15s
     this.gemsGroup = null;
     this.coconutGroup = null;
+    this.levelRank = 'beginner'; // default value
+    this.timer_01 = null;
+    this.timer_02 = null;
+    this.timer_03 = null;
+    this.time1 = 10000; // time beg
+    this.time2 = 10000; // time inter - 4min
+    this.time3 = 10000; // time expert - 6min
+    this.flag = 0;
 };
 
 /*
@@ -57,13 +65,13 @@ var fruits = function(game){
             this.gemsGroup.callAll('anchor.setTo','anchor',0.5,0.5);
            // this.gemsGroup.callAll('body.velocity.set','body.velocity',100,100);
             this.gemsGroup.callAll('body.setSize','body',38,40,5,2);
-            this.gemsGroup.callAll('body.gravity.set','body.gravity',0,300);
+            this.gemsGroup.callAll('body.gravity.set','body.gravity',0,330);
             this.gemsGroup.callAll('body.bounce.set','body.bounce',0.6);
             Phaser.ArrayUtils.shuffle(this.gemsGroup);
             this.gemsGroup.updateZ();
             
             this.coconutGroup = this.game.add.physicsGroup(Phaser.Physics.ARCADE);
-            this.coconutGroup.createMultiple(3,'coconut',false);
+            this.coconutGroup.createMultiple(4,'coconut',false);
             this.coconutGroup.callAll('body.setSize','body',35,40,5,2);
             //this.coconutGroup.callAll('body.velocity.set','body.velocity',100,100);
             this.coconutGroup.callAll('body.gravity.set','body.gravity',0,300);
@@ -75,8 +83,14 @@ var fruits = function(game){
             this.coconutGroup.setAll('body.checkCollision.up',false);
             
             game.time.events.loop(this.gemsLoopTime,this.handleGems,this); // loop to infinity
-            game.time.events.loop(this.cocoLoopTime,this.handleCoconuts,this); // loop to infinity
+            //game.time.events.loop(this.cocoLoopTime,this.handleCoconuts,this); // loop to infinity
             
+            // create timer for levelRank
+            this.timer_01 = game.time.create(false);
+            this.timer_02 = game.time.create(false);
+            this.timer_03 = game.time.create(false);
+            
+            this.setTimer(); 
         },
         
         initialFruits: function(){
@@ -122,27 +136,9 @@ var fruits = function(game){
                 var gem = this.gemsGroup.getFirstDead();
                 if(gem){
                 //this.y = game.rnd.integerInRange(this.min,this.min-500);
-                var y = game.world.bounds.y+game.rnd.integerInRange(1500,2000);    
+                var y = game.world.bounds.y+game.rnd.integerInRange(1500,1800);    
                 this.x = game.rnd.integerInRange(30,320);
                 gem.reset(this.x,y);
-               // return;
-            }
-          }
-        },
-        
-        handleCoconuts: function(){
-              var rand = game.rnd.integerInRange(0,2);
-           // console.log(rand);
-          for(var i=0;i<=rand;i++){
-                var coco = this.coconutGroup.getFirstDead();
-                if(coco){
-                //this.y = game.rnd.integerInRange(this.min,this.min-600);
-                var y = game.world.bounds.y+game.rnd.integerInRange(1500,2000);
-                this.x = game.rnd.integerInRange(30,320);
-                coco.reset(this.x,y);
-                    //console.log('y '+this.y);
-                    //console.log('cy '+game.camera.y);
-                    //console.log('gby '+game.world.bounds.y);
                // return;
             }
           }
@@ -154,16 +150,109 @@ var fruits = function(game){
             }
         },
         
+        handleCoconuts: function(){
+            var rand = game.rnd.integerInRange(0,3);
+           // console.log(rand);
+          for(var i=0;i<=rand;i++){
+                var coco = this.coconutGroup.getFirstDead();
+                if(coco){
+                //this.y = game.rnd.integerInRange(this.min,this.min-600);
+                var y = game.world.bounds.y+game.rnd.integerInRange(1500,1800);
+                this.x = game.rnd.integerInRange(30,320);
+                coco.reset(this.x,y);
+               // return;
+            }
+          }
+        },
+        
         killCoconut: function(coco){
             if(coco.y>game.height+game.camera.y){
                 coco.kill();
             }
         },
         
+        setTimer: function(){
+            this.timer_01.loop(this.time1,this.level_01,this);
+            this.timer_02.loop(this.time2,this.level_02,this);
+            this.timer_03.loop(this.time3,this.level_03,this);
+            console.log('timer set'); 
+            this.timer_01.start();
+            //this.timer_02.start();
+           // this.timer_03.start();
+            console.log('timer started'); 
+        },
+        
+        level_01: function(){
+            //console.log('level 1 o'); 
+            if(this.flag<=3){
+                var coco = this.coconutGroup.getFirstDead();
+                if(coco){
+                //this.y = game.rnd.integerInRange(this.min,this.min-600);
+                var y = game.world.bounds.y+game.rnd.integerInRange(1500,1800);
+                this.x = game.rnd.integerInRange(30,320);
+                coco.reset(this.x,y);
+                this.flag = this.flag+1;
+                console.log('level 1');     
+            }
+        }
+            
+        },
+        
+        level_02: function(){
+            
+              var rand = game.rnd.integerInRange(1,2);
+              for(var i=0;i<=rand;i++){
+                var coco = this.coconutGroup.getFirstDead();
+                if(coco){
+                //this.y = game.rnd.integerInRange(this.min,this.min-600);
+                var y = game.world.bounds.y+game.rnd.integerInRange(1500,1800);
+                this.x = game.rnd.integerInRange(30,320);
+                coco.reset(this.x,y);
+                this.flag = this.flag+1;
+                console.log('level 2');  
+            }
+          }
+            
+        },
+        
+        level_03: function(){
+            
+            var rand = game.rnd.integerInRange(2,3);
+              for(var i=0;i<=rand;i++){
+                var coco = this.coconutGroup.getFirstDead();
+                if(coco){
+                //this.y = game.rnd.integerInRange(this.min,this.min-600);
+                var y = game.world.bounds.y+game.rnd.integerInRange(1500,1800);
+                this.x = game.rnd.integerInRange(30,320);
+                coco.reset(this.x,y);
+                this.flag = this.flag+1;
+                console.log('level 3');  
+            }
+          }
+        
+            
+        },
+        
+        handleDifficulty : function(flag){
+            if(flag == 3){
+                this.timer_01.stop();
+               // console.log('timer 1 stopped');
+                //console.log('timer 2 started');
+                this.timer_02.start();
+            }
+            else if(flag >= 10){
+                this.timer_02.stop();
+                this.timer_03.start();
+            }
+            
+        },
+        
         update: function(){
              this.fruitsGroup.forEachAlive(this.handleFruits,this);
              this.gemsGroup.forEachAlive(this.killGems,this);
              this.coconutGroup.forEachAlive(this.killCoconut,this);
+             
+             this.handleDifficulty(this.flag); // manage time event   
         },
 
         render: function(){
