cedf2d7e7372ff6d7f670eba8edef3e0e1a3b94b
Md. Shohanur Rahaman <shohan4556@gmail.com>
2015-11-25 23:21:29 +0600

    play button tweening added

--- script/Menu.js
+++ script/Menu.js
@@ -11,11 +11,20 @@ var menuState = function(game){
             this.titleBg = game.add.sprite(game.world.centerX,game.world.centerY,'title-bg');
             this.titleBg.anchor.setTo(0.5,0.5);
             this.titleBg.scale.setTo(0.6,0.6);
+            
             game.global.menuBgSound = this.game.add.audio('menuBg',1,true);
             game.global.menuBgSound.play(); // replay from Button.js
             
             this.buttons = new menuButtons(game);
             this.buttons.create();
+            
+            this.tweenButton(this.buttons.playbtn); // make button juicy 
+        },
+        
+        tweenButton: function(button){
+            game.add.tween(button).to({
+               y:button.y+8,y:button.y-8 
+            },1000,Phaser.Easing.Linear.None,true,0,-1,true);
         }
         
     }
